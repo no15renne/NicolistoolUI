0fb8e9d2e0b4e2f043a1c5e8edf4c2063264de25
no15_renne <no15_renne@yahoo.co.jp>
2020-01-01 09:54:12 +0900

    mylist-itemのCSS作成

--- src/mylist.vue
+++ src/mylist.vue
@@ -1,53 +1,52 @@
 <template>
-	<div>
-		<h2 v-bind:style="fontStyle">{{ title }}</h2>
-    <input type="text" v-model="search">
-		<ul>
-			<li v-for="ml in filteredItems" :key="ml.id">
-				<span><p v-on:click="updateSelectedMylist(ml.id, ml.name)">{{ ml.name }}</p></span>
-				<span>{{ml.public ? "public" : "private" }}</span>
-			</li>
-		</ul>
-	</div>
+  <div>
+    <h2>{{ title }}</h2>
+    <input type="text" v-model="mylistSearch">
+    <ul>
+      <li v-for="ml in filteredItems" :key="ml.id">
+        <span><p v-on:click="updateSelectedMylist(ml.id, ml.name)">{{ ml.name }}</p></span>
+        <span>{{ml.public ? "public" : "private" }}</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-	import 'babel-polyfill';
-	import NocoLisToolClient from './util/nicolistool_client';
-	module.exports = {
-	  props: {
-	    updateSelectedMylist: Function
-	  },
-  	data: function() {
-	    return {
-	      title: "Nico MyLists Items!",
-	      fontStyle: {
-	          fontSize: "20px",
-	      },
-	      myLists: [],
-				myListsUrl: [],
-				search: ''
-	    }
-	  },
-	  created: function () {
-	    this.fetchData();
-	  },
-	  methods: {
-	    fetchData: async function() {
-				const client = new NocoLisToolClient();
-				let response = await client.getMyLists();
-				this.myLists = response;
-	    }
+  import 'babel-polyfill';
+  import NocoLisToolClient from './util/nicolistool_client';
+
+  const client = new NocoLisToolClient();
+
+  module.exports = {
+    props: {
+      updateSelectedMylist: Function
+  },
+    data: function() {
+      return {
+        title: "それコーンフレークやないかい！！！じゃあコーンフレーク違うかー！！！お嬢様ですわ。阪神また負けましたわ。",
+        myLists: [],
+        myListsUrl: [],
+        mylistSearch: ''
+      }
+    },
+    created: function () {
+      this.fetchData();
+    },
+    methods: {
+      fetchData: async function() {
+        const response = await client.getMyLists();
+        this.myLists = response;
+      }
     },
     computed: {
       filteredItems() {
-        if (this.search.toLowerCase() === "") {
+        if (this.mylistSearch.toLowerCase() === "") {
           return this.myLists;
         }
         return this.myLists.filter(item => {
-          return item.name.toLowerCase().indexOf(this.search.toLowerCase()) > -1
+          return item.name.toLowerCase().indexOf(this.mylistSearch.toLowerCase()) > -1
         })
       }
     }
-	}
+  }
 </script>
--- src/mylist_item.vue
+++ src/mylist_item.vue
@@ -1,81 +1,111 @@
 <template>
-	<div>
-		<h2 v-bind:style="fontStyle">{{ mylistName }}</h2>
-    <input type="text" v-model="search">
-		<ul>
-			<li v-for="item in filteredItems" :key="item.video_id">
-			  <div class="video_thumbnail">
-          <img :src=item.thumbnail_url>
+  <div>
+    <h2>{{ mylistName }}</h2>
+    <input type="text" v-model="mylistItemSearch">
+    <div id="grid" class="mylist-contents-list">
+      <div class="mylist-contents" v-for="item in filteredItems" :key="item.video_id">
+        <div class="video-thumbnail">
+          <img :src=item.thumbnail_url width="320" height="180">
         </div>
-				<span><a :href="getUrl(item.video_id)">{{ item.title }}</a></span>
-				<div class="video_tags">
-				  <ul>
-				    <li v-for="tag in item.tags">
-				      <span>{{ tag }}</span>
-				    </li>
-				  </ul>
-				</div>
-			</li>
-		</ul>
-	</div>
+        <div class="video-title">{{ item.title }}</div>
+        <div class="video-tags">
+            <div class="video-tag" v-for="tag in item.tags">
+              <span>{{ tag }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
+<style>
+.mylist-contents-list {
+}
+.mylist-contents {
+  width: 320px;
+  margin: 3px;
+  border-radius: 20px;
+  box-shadow: 0 0 2px rgba(0,0,0,.1);
+}
+.video-thumbnail {
+  width: 320px;
+}
+.video-thumbnail img {
+  width: 320px;
+  border-radius: 20px 20px 0px 0px;
+}
+.video-title {
+  padding: 6px;
+}
+.video-tags {
+  display:flex;
+  flex-wrap: wrap;
+  padding-bottom: 6px;
+}
+.video-tag {
+  padding: 3px;
+  font-size: 0.7em;
+  color: rgba(0, 0, 0, .3);
+}
+</style>
+
 <script>
-	import 'babel-polyfill';
-	import NocoLisToolClient from './util/nicolistool_client';
-	module.exports = {
-	  props: {
-	    'mylistId': String,
-	    'mylistName': String
-	  },
-  	data: function() {
-	    return {
-	      fontStyle: {
-          fontSize: "20px",
-	      },
-	      items: [],
-				search: ''
-	    }
-	  },
-	  created: function () {
-	    if (this.mylistId) {
-	      this.fetchData(this.mylistId);
-	    }
-	  },
-	  watch: {
+  import 'babel-polyfill';
+  import NocoLisToolClient from './util/nicolistool_client';
+  const client = new NocoLisToolClient();
+
+  module.exports = {
+    props: {
+      'mylistId'  : String,
+      'mylistName': String
+    },
+    data: function() {
+      return {
+        items: [],
+        mylistItemSearch: ''
+      }
+    },
+    created: function () {
+      if (this.mylistId) {
+        this.fetchData(this.mylistId);
+      }
+    },
+    updated: function() {
+      $('#grid').masonry('reloadItems').masonry();
+    },
+    watch: {
       mylistId: function(newMylistId, oldMylistId){
         if (newMylistId !== oldMylistId) {
           this.fetchData(newMylistId);
         }
       }
     },
-	  methods: {
-	    fetchData: async function(myListId) {
-				const client = new NocoLisToolClient();
-				let response = await client.getMyListItems(myListId);
-				await Promise.all(response.map(async res => {
-				  let tags_info = await client.getVideoTags(res.video_id.slice(2));
-				  res.tags = tags_info.tags;
-				}));
-				this.items = response;
-	    },
-			getUrl: function(id) {
-				return `https://www.nicovideo.jp/watch/${id}`;
-			}
+    methods: {
+      fetchData: async function(myListId) {
+        const response = await client.getMyListItems(myListId);
+        await Promise.all(response.map(async res => {
+          const tags_info = await client.getVideoTags(res.video_id.slice(2));
+          res.tags = tags_info.tags;
+        }));
+        this.items = response;
+      },
+      getUrl: function(id) {
+        return `https://www.nicovideo.jp/watch/${id}`;
+      }
     },
     computed: {
       filteredItems() {
-        if (this.search.toLowerCase() === "") {
+        if (this.mylistItemSearch === "") {
           return this.items;
         }
-        let res = this.items.filter(item => {
-          const word = this.search.toLowerCase();
+        return this.items.filter(item => {
+          const word = this.mylistItemSearch.toLowerCase();
           const likeTitle = item.title.toLowerCase().indexOf(word) > -1;
           const likeTags  = item.tags.some(tag => tag.toLowerCase().indexOf(word) > -1);
           return likeTitle || likeTags;
-        })
-        return res;
+        });
       }
     }
-	}
+  }
 </script>
